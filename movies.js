3ebb9c12bb8102a1619dd8f6414c56b3e3b78182
yryss <98253870+yryss@users.noreply.github.com>
2022-01-24 13:45:31 +0800

    Add files via upload

---
+++ movies.js
@@ -0,0 +1,64 @@
+function getMovies() {
+    return[
+        {
+            id:1,
+            type:"now",
+            thumbnail:"images/bigbuck.jpg",
+            src:"https://courses.cs.cityu.edu.hk/cs2204/video/bigbuck-s.mp4",
+            name:"BigBuckBunny",
+            cast:"Ton Roosendaal",
+            director:"Sacha Goedegebure",
+            duration:80
+        },
+        {
+            id:2,
+            type:"now",
+            thumbnail:"images/nature.jpg",
+            src:"https://courses.cs.cityu.edu.hk/cs2204/video/nature.mp4",
+            name:"Nature",
+            cast:"Jan Morgenstern",
+            director:"Barry Levinson",
+            duration:90
+        },
+        {
+            id:3,
+            type:"now",
+            thumbnail:"images/wonders.jpg",
+            src:"https://courses.cs.cityu.edu.hk/cs2204/video/wonders.mp4",
+            name:"Wonders",
+            cast:"Jacob Tremblay",
+            director:"Stephen Chbosky",
+            duration:120
+        },
+        {
+            id:4,
+            type:"upcoming",
+            thumbnail:"images/casablanca.png",
+            src:"https://courses.cs.cityu.edu.hk/cs2204/video/casablanca-s.mp4",
+            name:"Casablanca",
+            cast:"Humphrey Bogart",
+            director:"Michael Curtiz",
+            duration:130
+        },
+        {
+            id:5,
+            type:"upcoming",
+            thumbnail:"images/wildlife.jpg",
+            src:"https://courses.cs.cityu.edu.hk/cs2204/video/wildlife.mp4",
+            name:"Wildlife",
+            cast:"Carey Mulligan",
+            director:"Paul Dano",
+            duration:100
+        },
+        {
+            id:6,
+            type:"upcoming",
+            thumbnail:"images/chocolate.jpg",
+            src:"https://courses.cs.cityu.edu.hk/cs2204/video/chocolate.mp4",
+            name:"Chocolate",
+            cast:"Juliette Binoche",
+            director:"Vivek Agnihotri",
+            duration:60
+        }
+    ]
+}
